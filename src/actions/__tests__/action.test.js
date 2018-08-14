07bef472abc38b8e46ea6735270ce35153512655
khotz <kai.hotz@complevo.de>
2018-08-14 09:54:07 +0200

    - lint Clean

--- src/actions/__tests__/action.test.js
+++ src/actions/__tests__/action.test.js
@@ -3,11 +3,11 @@ import response from '../../mockedData/response.json'
 
 describe('Fetch Datat action', () => {
   it('Dispatches the correct action and payload', async () => {
-    const dispatch = jest.fn();
-    const getState = jest.fn();
+    const dispatch = jest.fn()
+    const getState = jest.fn()
 
-    await fetchData()(dispatch, getState);
-    expect(dispatch).toBeCalledWith({ type: "FETCH_DATA", payload: response });
-  });
-});
+    await fetchData()(dispatch, getState)
+    expect(dispatch).toBeCalledWith({ type: 'FETCH_DATA', payload: response })
+  })
+})
 
--- src/components/__tests__/select.test.jsx
+++ src/components/__tests__/select.test.jsx
@@ -27,13 +27,12 @@ describe('<Select />', () => {
   it('should be disableable', () => {
     const props = {
       ...baseProps,
-      label: 'customLabel'
+      label: 'customLabel',
     }
     const wrapper = mount(<Select {...props} />)
 
     expect(wrapper.find('label').text().includes(props.label)).toBeTruthy()
   })
-
 
   it('should be disableable', () => {
     const wrapper = mount(<Select disabled {...baseProps} />)
